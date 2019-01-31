1a4dd52e6891a0db5f5145982b763a6f92a7d0d6
Ethan Chan <metakirby5@gmail.com>
2019-01-30 17:43:01 -0800

    Update test style.

--- test/mpx/cli_test.rb
+++ test/mpx/cli_test.rb
@@ -1,93 +1,97 @@
-require "test_helper"
+require "minitest/autorun"
 require "mpx/cli"
 
-class CliTest < Minitest::Test
-  [
-    {
-      name: 'single_command',
-      input: %w(py:deps),
-      cmd: 'py',
-      args: %w(deps)
-    },
-    {
-      name: 'single_command_with_args',
-      input: %w(py:install zenbu),
-      cmd: 'py',
-      args: %w(install zenbu)
-    },
-    {
-      name: 'no_command',
-      input: %w(:deps),
-      cmd: nil,
-      args: %w(deps)
-    },
-    {
-      name: 'no_command_with_args',
-      input: %w(:install zenbu),
-      cmd: nil,
-      args: %w(install zenbu)
-    },
-    {
-      name: 'just_two_colons',
-      input: %w(::),
-      cmd: nil,
-      args: %w(:)
-    },
-    {
-      name: 'colons_1',
-      input: %w(a:b:c :d:),
-      cmd: 'a',
-      args: %w(b:c :d:)
-    },
-    {
-      name: 'colons_2',
-      input: %w(::a b:),
-      cmd: nil,
-      args: %w(:a b:)
-    },
-    {
-      name: 'colons_3',
-      input: %w(a:: b:),
-      cmd: 'a',
-      args: %w(: b:)
-    },
-  ].each do |c|
-    define_method("test_parses_#{c[:name]}") do
-      cmd, args = Mpx::Cli
-        .parse_args(c[:input])
-        .values_at(:cmd, :args)
+describe Mpx::Cli do
+  describe 'when input is valid' do
+    [
+      {
+        case: 'single command',
+        input: %w(py:deps),
+        cmd: 'py',
+        args: %w(deps)
+      },
+      {
+        case: 'single command with args',
+        input: %w(py:install zenbu),
+        cmd: 'py',
+        args: %w(install zenbu)
+      },
+      {
+        case: 'no command',
+        input: %w(:deps),
+        cmd: nil,
+        args: %w(deps)
+      },
+      {
+        case: 'no command with args',
+        input: %w(:install zenbu),
+        cmd: nil,
+        args: %w(install zenbu)
+      },
+      {
+        case: 'just two colons',
+        input: %w(::),
+        cmd: nil,
+        args: %w(:)
+      },
+      {
+        case: 'colons 1',
+        input: %w(a:b:c :d:),
+        cmd: 'a',
+        args: %w(b:c :d:)
+      },
+      {
+        case: 'colons 2',
+        input: %w(::a b:),
+        cmd: nil,
+        args: %w(:a b:)
+      },
+      {
+        case: 'colons 3',
+        input: %w(a:: b:),
+        cmd: 'a',
+        args: %w(: b:)
+      },
+    ].each do |c|
+      it "must parse #{c[:case]}" do
+        cmd, args = Mpx::Cli
+          .parse_args(c[:input])
+          .values_at(:cmd, :args)
 
-      if c[:cmd]
-        assert_equal c[:cmd], cmd
-      else
-        assert_nil c[:cmd]
+        if c[:cmd]
+          assert_equal c[:cmd], cmd
+        else
+          assert_nil c[:cmd]
+        end
+
+        assert_equal c[:args], args
       end
-
-      assert_equal c[:args], args
     end
   end
 
-  [
-    {
-      name: 'no_first_arg',
-      input: %w(py:)
-    },
-    {
-      name: 'no_first_arg_but_later_args',
-      input: %w(py: install)
-    },
-    {
-      name: 'no_directive',
-      input: %w(install)
-    },
-    {
-      name: 'just_colon',
-      input: %w(:),
-    },
-  ].each do |c|
-    define_method("test_argument_error_#{c[:name]}") do
-      assert_raises(ArgumentError) do
-        Mpx::Cli.parse_args(c[:input])
+  describe 'when input is invalid' do
+    [
+      {
+        case: 'no first arg',
+        input: %w(py:)
+      },
+      {
+        case: 'no first arg but later args',
+        input: %w(py: install)
+      },
+      {
+        case: 'no directive',
+        input: %w(install)
+      },
+      {
+        case: 'just colon',
+        input: %w(:),
+      },
+    ].each do |c|
+      it "must raise ArgumentError for #{c[:case]}" do
+        assert_raises(ArgumentError) do
+          Mpx::Cli.parse_args(c[:input])
+        end
       end
     end
   end
